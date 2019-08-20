73102b7dada49a1491f3d0bedccc93ea3e99f356
Jasyn Marais <jasynmarais@gmail.com>
2019-08-20 14:18:58 +0200

    closure master is sacred

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,13 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+const zen = 'Master is Sacred!';
+
+function returnZen(){
+    return zen;
+}
+
+console.log(returnZen());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
